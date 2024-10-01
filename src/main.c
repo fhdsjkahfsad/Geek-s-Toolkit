a4b9113a1ccd2ebd6262f2da9fba633ab1c50e00
Benjamin Liu <151111856+fhdsjkahfsad@users.noreply.github.com>
2024-10-01 18:46:38 +0000

    modified:   build/gtk 	modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdbool.h>
 int main(int argc, char* argv[]) {
     
     printf("Welcome to the toolkit for geeks!\n");
@@ -10,12 +10,16 @@ int main(int argc, char* argv[]) {
         printf("Error: could not allocate mem");
         exit(1);
     }
+    if (argv[1] == NULL) {
+        printf("Enter a name dummy\n");
+    }
     strcpy(name, argv[1]);
     printf("name at %p \n", &name);
     while(1) {
+        
         int opt;
         printf("Hey %s, What would you like to do?\n", name);
-        printf("opts:\n 42 to quit\n 1 to draw ghost\n 2 run command\n");
+        printf("opts:\n 42 to quit\n 1 to draw ghost\n 2 run command\n192 to check syslog (linux)\n");
         printf(">>>");
         scanf("%i", &opt);
         if (opt == 42) {
@@ -33,9 +37,29 @@ int main(int argc, char* argv[]) {
             } else {
                 printf("\033[0;32mSyscalls online.\033[0m\n");
             }
+        } else if (opt == 192) {
+            system("clear");
+            FILE *fp = popen("journalctl", "r");
+            if (fp == NULL) {
+                perror("Failed to run journalctl");
+                return EXIT_FAILURE;
+            }
+
+            char buffer[256];
+            while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+                printf("%s", buffer);
+            }
+
+            pclose(fp);
+    
+        
         } else {
             printf("could not find command %i\n", opt);
         }
+        
+        
+        
+        
     }
     free(name);
     name = NULL;
